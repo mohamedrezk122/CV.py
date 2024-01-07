a86e937dd13ced3c0ab03f7915db7440bf660431
Mohamed Rezk <mohrizq895@gmail.com>
2024-01-07 04:26:57 +0200

    parse functions for configs

--- config.py
+++ config.py
@@ -1,8 +1,22 @@
-# format id 
-FORMAT = 1 
+import yaml 
+from primitives import batch_replace
 
-# margin geometry(in cm)
-LEFT_MARGIN = 1.524
-RIGHT_MARGIN = 1.524
-TOP_MARGIN = 1.27
-BOTTOM_MARGIN = 1.524 
+DEFAULT_CONFIG_FILE = "config.yaml"
+
+def read_config(option:str) -> dict:
+    config_file_path = DEFAULT_CONFIG_FILE
+    with open(config_file_path , 'r') as file :
+        return yaml.safe_load(file)[option]    
+
+def adjust_document_margins(header:str)-> str:
+    margins = read_config("margins")
+    replacements = { "[[left]]"  : margins["left"] , 
+                     "[[right]]" : margins["right"],
+                     "[[top]]"   : margins["top"]  , 
+                     "[[bottom]]": margins["bottom"],
+                     "[[unit]]"  : margins["unit"]}
+    return batch_replace(header, replacements) 
+
+# format id 
+FORMAT = read_config("format")
+
