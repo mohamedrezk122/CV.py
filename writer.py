3945ea6cb678cbc98e117272b576cde465ea6c47
Mohamed Rezk <mohrizq895@gmail.com>
2024-01-07 04:28:12 +0200

    rename and rewrite handler file

---
+++ writer.py
@@ -0,0 +1,159 @@
+import re 
+from typing import Any
+
+from config import * 
+from reader import * 
+from primitives import * 
+
+
+INFO_SEP = " | "
+
+def write_info(info_dict:dict)-> str :
+    
+    info_template = load_format_file("info")
+
+    if info_dict.get("name",0):
+        name = info_dict["name"]
+    else:
+        name = "NAME PLACE HOLDER"
+        put_warning("No Name")
+
+    points_string ="" 
+    for title in info_dict["url"]:
+        points_string +=  latex_add_href(url = info_dict['url'][title] , title= title) +  INFO_SEP
+
+    for i, point in enumerate(info_dict) : 
+        if point  in {"name","url"} or not info_dict[point]:
+            continue
+        points_string += str(info_dict[point]) 
+        if i == len(info_dict) - 1:
+            break
+        points_string += INFO_SEP
+
+    replacements = {"[[points]]" : points_string,
+                    "[[name]]"   : name}
+
+    return batch_replace(info_template,replacements)
+
+def write_section(title:str) -> str:
+    section_template = load_format_file("section")
+    return section_template.replace('[[title]]' , title)
+
+def write_url(entry:dict, content:str) -> str:
+    if not entry.get("url", 0):
+        return content
+    for title in entry["url"] :
+        link = latex_add_href(url = entry["url"][title] , title = title)
+        content = content.replace(f"[{title}]" , link)
+    return content
+
+def write_generic_entry( title:str,  year:str, org:str="", points:dict=None )-> str:
+    
+    if not points :
+        return ""
+
+    entry_template = load_format_file("entry")
+    org = latex_add_new_line(latex_emphasize(org))
+    points_string = "".join([latex_add_new_line(points[point]) for point in points])
+
+    replacements = { "[[title]]" : title , 
+                     "[[year]]"  : year  ,
+                     "[[org]]"   : org   ,
+                     "[[points]]": points_string}
+
+    return batch_replace(entry_template, replacements)
+
+def write_column_entry(width:int , points:dict) -> str:
+
+    if not points :
+        return ""  
+
+    col_template = load_format_file("col")
+    
+    points_string = "".join([latex_add_new_line(points[point]) for point in points])
+
+    replacements = { "[[col_width]]" : width , 
+                     "[[points]]"    : points_string}
+
+    return batch_replace(col_template, replacements) 
+
+def write_multi_column_entry(ncols:int, points:dict) -> str:
+
+    multi_col_template = ""
+    idx = len(points) // ncols 
+
+    for i in range(ncols):
+        u = idx*(i+1) if i < ncols-1 else len(points)
+        multi_col_template += write_column_entry(1/ncols , 
+                                    dict(list(points.items())[idx*i:u]))
+
+    return multi_col_template
+
+
+def get_value_or_default(field:dict , key:str , default:Any = "") -> Any:
+    value =  default
+    if field.get(key, 0):
+        value =  field[key]
+    return value  
+
+def append_multicol(subfield: dict, tex_content:str) -> str:
+    cols = get_value_or_default(subfield, "cols", 3)
+    points = get_value_or_default(subfield, "points", None)
+    return tex_content + write_multi_column_entry(cols , points)
+
+def append_entry(subfield:dict, tex_content:str) -> str:                            
+    year = get_value_or_default(subfield, "year")
+    org = get_value_or_default(subfield, "org")
+    points = get_value_or_default(subfield, "points", None)
+    entry_content  = write_generic_entry(subfield["title"] , year ,  org , points)
+    entry_content  = write_url(subfield , entry_content)
+    return tex_content + entry_content 
+
+def write_tex_file(file_content:dict)-> str :
+    
+    section_pattern = re.compile(r'section\d+')
+    entry_pattern = re.compile(r'entry\d+')
+    multicol_pattern = re.compile(r'multicol\d+')
+    
+    file_header = load_format_file("header")
+    file_header = adjust_document_margins(file_header)
+    tex_content = ""
+    
+    if file_content.get("info", 0):
+        tex_content  += write_info(file_content['info'])
+    else:
+        put_warning("No information section")
+
+    try:
+        file_content.pop('info')
+        for field in file_content : 
+            if not bool(section_pattern.match(field)):
+                continue 
+
+            tex_content += write_section(file_content[field]["title"])
+            if len(field) < 2 :
+                continue  
+
+            for sub in file_content[field] :
+                subfield    =  file_content[field][sub] 
+                if bool(entry_pattern.match(sub)):
+                    tex_content = append_entry(subfield, tex_content)
+                elif bool(multicol_pattern.match(sub)):
+                    tex_content = append_multicol(subfield, tex_content)
+
+    except:
+        put_error("not able to process yaml data, please check the file")
+
+    return file_header.replace("[[content]]" , tex_content)
+
+
+
+def export_tex_file(content:str , file_path:str) :
+    
+    try:
+        with open(f"{file_path}", "w") as file: 
+            file.write(content)
+            put_success(f"Your texfile has been written sucessfully to:\n {file_path}")
+    except:
+        put_error("cannot access directory to write your texfile")
+
